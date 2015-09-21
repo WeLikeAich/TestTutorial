db9409e1fa8dfa10addb1f20ad747cbe6fcca2ba
WeLikeAich <jaichler13@apu.edu>
2015-09-21 11:35:01 -0700

    test commit

---
+++ prizewheel_test.ino
@@ -0,0 +1,25 @@
+const int button = 13;
+const int motor = 8;
+int delayTime = 0;
+
+void setup() {
+  pinMode(motor, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(button) == HIGH)
+  {
+  	delayTime = random(8, 15);
+  	delayTime *= 1000;
+  	
+    digitalWrite(motor, HIGH);
+    delay(delayTime);
+    digitalWrite(motor, LOW);
+  }
+  else
+  {
+   digitalWrite(motor, LOW); 
+  }
+}
---
+++ sketch_sep08a/sketch_sep08a.ino
@@ -0,0 +1,38 @@
+int delayTime = 5;
+int button = 2;
+int motorPin = 3;
+
+void setup()
+{
+  pinMode(motorPin, OUTPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println(delayTime);
+  Serial.println(digitalRead(button));
+}
+
+void loop()
+{
+  Serial.println(digitalRead(button));
+  Serial.println(delayTime);  
+
+  Serial.println(delayTime);
+    
+    if(digitalRead(button) == HIGH)
+    {
+      delayTime = random(3,10);
+      delayTime *= 1000;
+      
+      Serial.println(digitalRead(button));
+      Serial.println(delayTime);
+      
+      analogWrite(motorPin, 80);
+      delay(delayTime);
+      analogWrite(motorPin, 0);
+    }
+    else
+    {
+     analogWrite(motorPin, 0); 
+    }
+}
